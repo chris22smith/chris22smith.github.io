3d5ce638976b5cceb8e799736e9cb21bca887c05
Chris Smith <chris22smith@gmail.com>
2023-02-05 12:49:30 +0000

    _redirects passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/_redirects");
   return {
     dir: {
       input: "src",
