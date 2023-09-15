8de3c003872c52b3d9539f59d82fe3b1f84dba12
Chris Smith <chris22smith@gmail.com>
2023-09-15 10:18:45 +0100

    fix photo ratio on mobile

--- _site/sw.js
+++ _site/sw.js
@@ -1,5 +1,5 @@
-const cacheStaticName = "static-v3";
-const cacheDynamicName = "dynamic-v1";
+const cacheStaticName = "static-v6";
+const cacheDynamicName = "dynamic-v3";
 
 self.addEventListener("install", (event) => {
   console.log("[Service Worker] Installing service worker...", event);
